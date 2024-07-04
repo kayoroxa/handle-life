bd2ef7ba26d1839f4a7ca53d60f0652fff6c5cfb
kayoroxa <kayoroxa@gmail.com>
2024-07-04 20:01:34 -0300

    init

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,74 @@
+'use client'
+import { serverLogin } from '@/app/actions'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { useToast } from '@/components/ui/use-toast'
+import { signIn } from 'next-auth/react'
+import { redirect } from 'next/navigation'
+import { useEffect } from 'react'
+
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { email?: string }
+}) {
+  const { toast } = useToast()
+  const userEmailDefault = searchParams?.email
+  async function handleSubmit(formData: FormData) {
+    const email = formData.get('email') as string
+    const result = await serverLogin({ email })
+
+    if (result === false) {
+      toast({
+        title: 'Você não tem acesso ao curso.',
+        description: 'Me chame no whatsapp',
+      })
+      return
+    } else {
+      await signIn('credentials', {
+        email: email,
+        redirect: true,
+        callbackUrl: '/',
+      })
+      redirect('/')
+    }
+  }
+
+  useEffect(() => {
+    async function handleAutoSubmit(email: string) {
+      const result = await serverLogin({ email })
+
+      if (result === false) {
+        toast({
+          title: 'Você não tem acesso ao curso.',
+          description: 'Me chame no whatsapp',
+        })
+        return
+      } else {
+        await signIn('credentials', {
+          email: email,
+          redirect: true,
+          callbackUrl: '/',
+        })
+        redirect('/')
+      }
+    }
+
+    if (userEmailDefault) {
+      handleAutoSubmit(userEmailDefault)
+    }
+  }, [toast, userEmailDefault])
+
+  return (
+    <div>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <form action={handleSubmit} className="flex flex-col gap-4 w-[400px]">
+          <h1>Log In</h1>
+          <Input type="email" name="email" placeholder="email" required />
+          {/* <input type="password" placeholder="Password" required /> */}
+          <Button>Log In</Button>
+        </form>
+      </div>
+    </div>
+  )
+}
---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,60 @@
+import { prisma } from '@/lib/prisma'
+import { NextAuthOptions } from 'next-auth'
+import NextAuth from 'next-auth/next'
+import CredentialsProvider from 'next-auth/providers/credentials'
+
+const handler: NextAuthOptions = NextAuth({
+  pages: {
+    signIn: '/login',
+  },
+
+  providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+
+      credentials: {
+        email: {
+          label: 'email',
+          type: 'email',
+          placeholder: 'seu_email@exemplo.com',
+        },
+        // password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials, req) {
+        if (!credentials) return null
+
+        const { email } = credentials
+
+        const user = await prisma.user.findFirst({
+          where: {
+            email,
+            blocked: false,
+          },
+        })
+
+        if (user) {
+          console.log(credentials)
+
+          return {
+            id: String(user.id),
+            email,
+            name: user.name,
+          }
+        }
+        return null
+      },
+    }),
+  ],
+  // callbacks: {
+  //   session({ session, user }) {
+  //     if (session.user) {
+  //       session.user.id = user.id
+  //       return session
+  //     }
+
+  //     return session
+  //   },
+  // },
+})
+
+export { handler as GET, handler as POST }
