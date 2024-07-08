23c7f7a407b46f86f5a657e15dcffded0462c714
kayoroxa <kayoroxa@gmail.com>
2024-07-08 12:08:43 -0300

    auto easy index bat

--- index.bat
+++ index.bat
@@ -1,2 +1,20 @@
+@echo off
+
+REM Atualiza as referências do repositório remoto
+git fetch
+
+REM Obtém o nome da branch atual
+FOR /F "tokens=*" %%i IN ('git symbolic-ref --short HEAD') DO SET CURRENT_BRANCH=%%i
+
+REM Obtém o número de commits que precisam ser puxados
+FOR /F "tokens=*" %%i IN ('git rev-list --count %CURRENT_BRANCH%...origin/%CURRENT_BRANCH%') DO SET COUNT=%%i
+
+REM Verifica se COUNT é maior que 0
+IF %COUNT% GTR 0 (
+    echo Updating... there are %COUNT% changes to be changes to be pulled.
+    call yarn spb
+)
+
+REM Inicia o navegador e o servidor
 start msedge http://localhost:6700
-yarn start
+call yarn start
